6573ef67c6ea8cb05e0d73cb51dad33dabae20cb
robert-ionescu <92181252+robert-ionescu@users.noreply.github.com>
2023-03-04 20:39:05 +0200

    1st commit

---
+++ app/src/main/java/com/example/almightyapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.almightyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //private static String ip = "192.168.137.1";// this is the host ip that your data base exists on you can use 10.0.2.2 for local host                                                    found on your pc. use if config for windows to find the ip if the database exists on                                                    your pc
+    private static String ip = "10.0.2.2";// this is the host ip that your data base exists on you can use 10.0.2.2 for local host                                                    found on your pc. use if config for windows to find the ip if the database exists on                                                    your pc
+    private static String port = "5432";
+    private static String Classes = "org.postgresql.Driver";
+    private static String database = "almightyDB";
+    private static String username = "postgres";
+    private static String password = "postgres";
+    private static String url = "jdbc:postgresql://"+ip+":"+port+"/"+database;
+    private Connection connection = null;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void start(View view) {
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            Class.forName(Classes);
+            connection = DriverManager.getConnection(url, username,password);
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Class fail", Toast.LENGTH_SHORT).show();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Connected no", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
